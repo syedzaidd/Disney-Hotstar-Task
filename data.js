8a4aaf495b98081a88e7122c9d79fa00bd0e5f90
syedzaidd <162634039+syedzaidd@users.noreply.github.com>
2024-03-28 08:32:52 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Six months after being handed the mantle of Captain America at the end of Avengers: Endgame (2019), Sam Wilson teams up with Bucky Barnes on a global mission to stop an anti-patriotism group',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des: 'Becoming the God of Stories, Loki is now a living Temporal Loom holding all the branches together. To that end, Loki has transformed the timelines into a true multiverse tree, complete with roots and actual branches with Loki himself at the center keeping it all growing and alive while sitting on a new throne',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Schaeffer was excited to use the series to give social commentary while telling a story about character and grief, as she found Wandas grief to be relatable. A grief counselor was consulted, and Shakman said the entire series was about Wanda learning to process and overcome her grief.',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'The film follows Raya, a warrior princess who must find the fabled last dragon in order to save her divided home world from a ravenous plague. Throughout her journey, Raya traverses the various kingdoms of Kumandra, with an unusual band of misfits by her side.',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: ' Luca Paguro is a pre-teen sea monster living off the coast of the Italian city of Portorosso who spends his days herding a school of fish. His parents, Daniela and Lorenzo, restrict him from going to the surface out of fear, but doing the same thing day after day ends up making Luca bored.',
+        image: 'images/slider 5.PNG'
+    }
+]
